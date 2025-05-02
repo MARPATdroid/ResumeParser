37b711bef429d8dff42c8cfa4f6615b85ff878db
MARPATdroid <MARPATdroid@MARPATdroid.com>
2025-05-02 14:58:02 -0400

    add README
    add customizable input for
    file names
    color options
    and evaluation thresholds

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ import jobListing.JobListing;
 import resume.Resume;
 
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class main {
 
@@ -24,19 +25,61 @@ public class main {
     private int expDesires = 0;
     private int expOthers = 0;
 
-    private double threshold = 60.0;
+    private static double threshold = 60.0;
 
 
     public static void main(String[] args) {
 
-        main m = new main();
-        Resume resume = FileReader.readResume("Resume2.txt");
-        JobListing jobListing = FileReader.jobPosting("JobPosting.txt");
+        main m = new main();   //instatiate main function
+        String resumeFileName = "Resume.txt";
+        String jobListingFileName = "JobPosting.txt";
+        boolean color = true;
+        Scanner sc = new Scanner(System.in);
+        String colorChoice = "";
+
+
+        System.out.println("Welcome to Resume Parser!");
+        System.out.println("By MARPATdroid");
+        System.out.println("---------------------------------------------");
+        System.out.println("Please enter the file name of the resume you would like to parse: ");
+        resumeFileName = sc.nextLine();
+        System.out.println("Please enter the file name of the job listing you would like to parse: ");
+        jobListingFileName = sc.nextLine();
+
+        while (!colorChoice.equals("y") && !colorChoice.equals("n")) {
+            System.out.println("Would you like color highlights?  Works with VirtualTerminalLevel 1 or higher");
+            System.out.println("Y/N (if you experience wierd characters try turning off color):");
+            colorChoice = sc.nextLine();
+            if (colorChoice.toLowerCase().equals("y")) {
+                colorChoice = colorChoice.toLowerCase();
+                color = true;
+            } else if (colorChoice.toLowerCase().equals("n")) {
+                colorChoice = colorChoice.toLowerCase();
+                color = false;
+            }
+        }
+            //Get user input for manual threshold setting
+        int thres = -1;
+        while (thres < 0 || thres > 100) {
+            System.out.println("What matching threshold would you like to target(1 - 100%): ");
+            try {
+                thres = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid entry, please try again");
+            }
+        }
+        threshold = thres;
+
+
+
+
+        Resume resume = FileReader.readResume(resumeFileName);    //define the resume to be read in
+        JobListing jobListing = FileReader.jobPosting(jobListingFileName);        //define the joblisting to be read in
 
 
         m.compareSkills(resume, jobListing);
         m.compareExperience(resume, jobListing);
-        m.printResults(resume);
+        m.printResults(resume, color);
         m.printWordsMissing(resume, jobListing);
 
         System.out.println();
@@ -87,12 +130,12 @@ public class main {
                         }
                     }
                 }
-                score = count / splitS2.length;
-                if ((score * 100) > threshold) {
+                score = count / splitS2.length;         //calculate scores
+                if ((score * 100) > threshold) {        //if they are higher than threshold increment and store in the matched lines array
                     skillDesires += 1;
                     resume.setMatchedLine(s, s2);
                 }
-                resume.setSkillScores(s, score, "Desires");
+                resume.setSkillScores(s, score, "Desires");       // store the scores properly
             }
             for (String s2 : jobListing.getOthers()) {  //get each line from the job listing
                 String[] splitS = s.trim().toLowerCase().split(regex);  //split each line into words all lowercase with no extra whitespace
@@ -107,12 +150,12 @@ public class main {
                         }
                     }
                 }
-                score = count / splitS2.length;
-                if ((score * 100) > threshold) {
+                score = count / splitS2.length;         //calculate scores
+                if ((score * 100) > threshold) {    //if they are higher than threshold increment and store in the matched lines array
                     skillOthers += 1;
                     resume.setMatchedLine(s, s2);
                 }
-                resume.setSkillScores(s, score, "Others");
+                resume.setSkillScores(s, score, "Others");// store the scores properly
             }
 
         }
@@ -140,12 +183,12 @@ public class main {
                         }
                     }
                 }
-                score = count / splitS2.length;
-                if ((score * 100) > threshold) {
+                score = count / splitS2.length;         //calculate scores
+                if ((score * 100) > threshold) {    //if they are higher than threshold increment and store in the matched lines array
                     expNeeds += 1;
                     resume.setMatchedLine(s, s2);
                 }
-                resume.setExpScores(s, score, "Needs");
+                resume.setExpScores(s, score, "Needs");// store the scores properly
             }
             for (String s2 : jobListing.getDesires()) {  //get each line from the job listing
                 String[] splitS = s.trim().toLowerCase().split(regex);  //split each line into words all lowercase with no extra whitespace
@@ -160,12 +203,12 @@ public class main {
                         }
                     }
                 }
-                score = count / splitS2.length;
-                if ((score * 100.00) > threshold) {
+                score = count / splitS2.length;         //calculate scores
+                if ((score * 100.00) > threshold) {    //if they are higher than threshold increment and store in the matched lines array
                     expDesires += 1;
                     resume.setMatchedLine(s, s2);
                 }
-                resume.setExpScores(s, score, "Desires");
+                resume.setExpScores(s, score, "Desires");// store the scores properly
             }
             for (String s2 : jobListing.getOthers()) {  //get each line from the job listing
                 String[] splitS = s.trim().toLowerCase().split(regex);  //split each line into words all lowercase with no extra whitespace
@@ -180,12 +223,12 @@ public class main {
                         }
                     }
                 }
-                score = count / splitS2.length;
-                if ((score * 100) > threshold) {
+                score = count / splitS2.length;         //calculate scores
+                if ((score * 100) > threshold) {    //if they are higher than threshold increment and store in the matched lines array
                     expOthers += 1;
                     resume.setMatchedLine(s, s2);
                 }
-                resume.setExpScores(s, score, "Others");
+                resume.setExpScores(s, score, "Others");// store the scores properly
             }
 
         }
@@ -194,10 +237,10 @@ public class main {
 
     /**
      * simple display method
-     *
+     * for each line in the resume will color code them in ANSI display
      * @param resume to be displayed
      */
-    private void printResults(Resume resume) {
+    private void printResults(Resume resume, Boolean color) {
         DecimalFormat f = new DecimalFormat("#.##");
 
         System.out.println("---------------------------------------------");
@@ -207,17 +250,21 @@ public class main {
         System.out.println("skill Desires: " + skillDesires);
         System.out.println("skill Others: " + skillOthers);
         for (String s : resume.getSkills()) {
-            if (resume.getSkillScores().get(s) * 100 < 40.0) {
-                System.out.print(ANSI_RED);
-            } else if (resume.getSkillScores().get(s) * 100 < 60.0) {
-                System.out.print(ANSI_YELLOW);
-            } else if (resume.getSkillScores().get(s) * 100 < 80.0) {
-                System.out.print(ANSI_BLUE);
-            } else {
-                System.out.print(ANSI_GREEN);
+            if (color) {
+                if (resume.getSkillScores().get(s) * 100 < 40.0) {
+                    System.out.print(ANSI_RED);
+                } else if (resume.getSkillScores().get(s) * 100 < 60.0) {
+                    System.out.print(ANSI_YELLOW);
+                } else if (resume.getSkillScores().get(s) * 100 < 80.0) {
+                    System.out.print(ANSI_BLUE);
+                } else {
+                    System.out.print(ANSI_GREEN);
+                }
             }
             System.out.println(s + " || Match " + f.format(resume.getSkillScores().get(s) * 100) + "% of type " + resume.getSkillType().get(s));
-            System.out.print(ANSI_RESET);
+            if(color) {
+                System.out.print(ANSI_RESET);
+            }
             if (resume.getSkillScores().get(s) * 100 > threshold) {
                 for (String s2 : resume.getMatchedLine().get(s)) {
                     System.out.println("          " + s2);
@@ -234,17 +281,21 @@ public class main {
 
         for (String s : resume.getExperience()) {
 
-            if (resume.getExpScores().get(s) * 100 < 40.0) {
-                System.out.print(ANSI_RED);
-            } else if (resume.getExpScores().get(s) * 100 < 60.0) {
-                System.out.print(ANSI_YELLOW);
-            } else if (resume.getExpScores().get(s) * 100 < 80.0) {
-                System.out.print(ANSI_BLUE);
-            } else {
-                System.out.print(ANSI_GREEN);
+            if (color) {
+                if (resume.getExpScores().get(s) * 100 < 40.0) {
+                    System.out.print(ANSI_RED);
+                } else if (resume.getExpScores().get(s) * 100 < 60.0) {
+                    System.out.print(ANSI_YELLOW);
+                } else if (resume.getExpScores().get(s) * 100 < 80.0) {
+                    System.out.print(ANSI_BLUE);
+                } else {
+                    System.out.print(ANSI_GREEN);
+                }
             }
             System.out.println(s + " || Match " + f.format(resume.getExpScores().get(s) * 100) + "% of type " + resume.getExpType().get(s));
-            System.out.print(ANSI_RESET);
+            if(color) {
+                System.out.print(ANSI_RESET);
+            }
             if (resume.getExpScores().get(s) * 100 > threshold) {
                 for (String s2 : resume.getMatchedLine().get(s)) {
                     System.out.println("          " + s2);
